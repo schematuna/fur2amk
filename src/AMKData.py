50f10c4613d6ea116d4ef30b5c94a96d78f66df4
schema tuna <schematuna@gmail.com>
2025-12-11 23:20:49 -0500

    It's alive again

--- src/AMKData.py
+++ src/AMKData.py
@@ -2,8 +2,6 @@ from dataclasses import dataclass, field
 from enum import Enum, auto
 from typing import Dict, List, Tuple, Any, Optional
 
-from AMKData import EventTable
-
 class AMKRemoteCommandType(Enum):
     GAIN = 0
 
@@ -20,7 +18,7 @@ class EventType(Enum):
     NOTE_OFF = auto()
     INS_CHANGE = auto()
     VOLUME = auto()
-    EFFECT = auto()
+    PITCH_BEND = auto()
 
 @dataclass
 class AMKRemoteCommand:
@@ -30,32 +28,43 @@ class AMKRemoteCommand:
     remote_command_arg: Optional[Any] = None
     amk_command_args: List[Any] = field(default_factory=list)
 
+@dataclass
+class AMKEnvelope:
+    attack: int = 0
+    decay: int = 0
+    sustain: int = 0
+    release: int = 0
 
+@dataclass
 class AMKInstrument:
-    def __init__(self, index: int, sample_index: int = None, is_noise: bool = False, noise_freq: int = 0) -> None:
-        self.index = index
-        self.is_noise = is_noise
-        if is_noise:
-            self.noise_freq = noise_freq
-            self.sample_index = None
-        else:
-            self.sample_index = sample_index
-            self.noise_freq = 0
-        self.remote_commands: List[AMKRemoteCommand] = []
+    sample_index: Optional[int] = None
 
-    @classmethod
-    def noise(cls, index: int, noise_freq: int) -> 'AMKInstrument':
-        return cls(index=index, is_noise=True, noise_freq=noise_freq)
+    is_noise: bool = False
+    noise_freq: int = 0
 
-    @classmethod
-    def sample(cls, index: int, sample_index: int) -> 'AMKInstrument':
-        return cls(index=index, sample_index=sample_index, is_noise=False)
+    uses_envelope: bool = False
+    envelope: Optional[AMKEnvelope] = None
+
+    gain_values: List[int] = field(default_factory=list)
+
+    gain: int = 0
+
+@dataclass
+class AMKEchoData:
+    firIdx: Optional[bool] = None
+    echoDelay: Optional[int] = None
+    echoFeedback: Optional[int] = None
+    echoMask: Optional[int] = None
+    echoVolL: Optional[int] = None
+    echoVolR: Optional[int] = None
+    echoFilterCoeffs: Optional[List[int]] = None
 
 @dataclass
 class Event:
     tick: int
-    effect: str
+    type: EventType
     value: Any
+    value2: Any = None
 
 @dataclass
 class EventTable:
@@ -63,23 +72,40 @@ class EventTable:
     intro_order: Optional[int] = None
 
 @dataclass
-class AMKData:
-    version: int = 2
+class SPCInfo:
     title: str = ""
     author: str = ""
     game: str = ""
     length: int = 0
     comment: str = ""
 
+@dataclass
+class AMKData:
+    version: int = 2
+
+    spc_info: SPCInfo = field(default_factory=SPCInfo)
+
     sample_path: str = ""
     # index -> filename, tuning string
-    samples: Dict[int, Tuple[str, str]] = {}
-    # List of (instrument_index, sample_index) pairs to emit in #instruments
-    instruments: List[Tuple[int, AMKInstrument]] = []
+    samples: Dict[int, Tuple[str, str]] = field(default_factory=dict)
+    instruments: List[AMKInstrument] = field(default_factory=list)
 
+    tempo: int = 0
+    volume: int = 0
+
+    echo_data: AMKEchoData = None
+
+    remote_commands: List[AMKRemoteCommand] = field(default_factory=list)
+
+    num_channels: int = 8
     # Events for MML emission
-    event_table: EventTable = None
+    event_table: EventTable = field(default_factory=EventTable)
     # which order to place the intro marker before
     intro_order: int = None
+
+    # song data for formatting
+    pattern_length: int = 0
+    measure_length: int = 0
+    ticks_per_beat: int = 0  # Speed1 * measure_length (ticks per row * rows per beat)
 
     label_start: int = 1
