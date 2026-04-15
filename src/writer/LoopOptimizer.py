e824128856aaf11c82685859ba174966f01342fc
schema tuna <schematuna@gmail.com>
2026-04-14 23:57:22 -0400

    Start in on better loop optimization.

--- src/writer/LoopOptimizer.py
+++ src/writer/LoopOptimizer.py
@@ -1,4 +1,5 @@
 from typing import List
+import copy
 
 from .MMLWriterData import *
 
@@ -30,5 +31,114 @@ class LoopOptimizer:
                 for lbl, section2 in labels_assigned.items():
                     if section2 == section:
                         section.loopInfo = [LoopInfo(range(len(section.sentences)), lbl, True)]
+                        break
+        return label_count
+    
+    @staticmethod
+    def behead_sentence(sentence: MMLSentence) -> Tuple[MMLSentence, List[MMLCommand]]:
+        sentence_local = copy.deepcopy(sentence)
+
+        first_word = sentence.words[0]
+        initial_commands: List[MMLCommand] = []
+        rem: List[MMLCommand] = []
+        for cmd in first_word.commands:
+            if cmd.tick == first_word.tick:
+                initial_commands.append(cmd)
+            else:
+                rem.append(cmd)
+        
+        # change first word commands to just commands not on first tick
+        sentence_local.words[0].commands = rem
+
+        return sentence_local, initial_commands
+    
+    @staticmethod
+    def rehead_sentence(sentence: MMLSentence, cmds: List[MMLCommand]) -> MMLSection:
+        for cmd in cmds:
+            sentence.words[0].commands.insert(0, cmd)
+
+        return sentence
+
+    
+    @staticmethod 
+    def fudge_equality(group1: List[MMLSentence], group2: List[MMLSentence]) -> Tuple[bool, List[MMLSentence], List[MMLSentence]]:
+        '''Compares two MML sections, returns whether they're equal.
+           If equal, also returns new MMLSections with unique initial commands split off into
+           a new initial sentence. '''
+        
+        group1_local = copy.deepcopy(group1)
+        group2_local = copy.deepcopy(group2)
+
+        group1_local[0], initial_cmds1 = LoopOptimizer.behead_sentence(group1_local[0])
+        group2_local[0], initial_cmds2 = LoopOptimizer.behead_sentence(group2_local[0])
+
+        # check for equality without initial commands
+        if group1_local == group2_local:
+            common_cmds = [item for item in initial_cmds1 if item in initial_cmds2]
+
+            # rebuild sections with split initial commands
+            if len(common_cmds) > 0:
+                group1_local[0] = LoopOptimizer.rehead_sentence(group1_local[0], common_cmds)
+                group2_local[0] = LoopOptimizer.rehead_sentence(group2_local[0], common_cmds)
+
+            unique1 = [item for item in initial_cmds1 if item not in initial_cmds2]
+            unique2 = [item for item in initial_cmds2 if item not in initial_cmds1]
+
+            if len(unique1) > 0:
+                initial_word1 = MMLWord(group1_local[0].words[0].tick, 0, None, unique1)
+                group1_local.insert(0, MMLSentence([initial_word1]))
+
+            if len(unique2) > 0:
+                # don't allow this case for now
+                # would need to accommadate in optimization algo
+                return False, None, None
+                initial_word2 = MMLWord(group2_local[0].words[0].tick, 0, None, unique2)
+                group2_local.insert(0, MMLSentence([initial_word2]))
+
+            return True, group1_local, group2_local
+        else:
+            return False, None, None
+
+
+    def optimize_loops_v2(self, sections: List[MMLSection], label_count: int) -> int:
+        # Identify and label loops in the channel lines
+        labels_assigned: Dict[int, List[MMLSentence]] = {}
+        unique_groups: Dict[int, List[MMLSentence]] = {}
+        for i, section in enumerate(sections):
+            group = section.sentences
+            # Check if this section matches any prior seen sections
+            # accounting for any extra commands at start of first section
+            matched_sec = None
+            for grp in unique_groups.values():
+                if self.fudge_equality(grp, group)[0]:
+                    matched_sec = grp
+                    break
+            if matched_sec is None:
+                unique_groups[i] = group
+                section.loopInfo = [LoopInfo(range(len(group)))]
+            elif group not in labels_assigned.values():
+                # Assign a label to this repeated pattern
+                labels_assigned[label_count] = group
+                section.loopInfo = [LoopInfo(range(len(group)), label_count, True)]
+                # and mark the first occurrence
+                for order, uniq_grp in unique_groups.items():
+                    eq, uniq_grp, grp = self.fudge_equality(uniq_grp, group)
+                    if eq:
+                        # update sections with potential new first sentence
+                        sections[order].sentences = uniq_grp
+                        section.sentences = grp
+                        # and mark initial unique section with label
+                        if uniq_grp[0].words[0].duration == 0:
+                            sections[order].loopInfo.insert(0, LoopInfo([0]))
+                            sections[order].loopInfo[1].sentenceIndices = range(1, len(uniq_grp))
+                        sections[order].loopInfo[-1].label = label_count
+                        break
+                label_count += 1
+            else:
+                # Find the existing label for this pattern
+                for lbl, assigned_grp in labels_assigned.items():
+                    eq, _, _ = self.fudge_equality(assigned_grp, group)
+                    if eq:
+                        section.loopInfo = [LoopInfo(range(len(group)), lbl, True)]
                         break
         return label_count
--- src/writer/MMLWriter.py
+++ src/writer/MMLWriter.py
@@ -253,7 +253,7 @@ class MMLWriter:
             sections.append(MMLSection(section_words, cur_section_num, self.mml_data.measure_length))
 
             optimizer = LoopOptimizer()
-            self.label_count = optimizer.optimize_loops(sections, self.label_count)
+            self.label_count = optimizer.optimize_loops_v2(sections, self.label_count)
 
             mml_state = MMLState()
             # light staccato is a global toggle
